a7645346dc03e02c12c98d37a52e22160114c805
abid7 <abidshahriar7@gmail.com>
2020-07-12 23:41:39 +0600

    made some minor changes

--- src/components/homepage-menu/homepage-menu.component.jsx
+++ src/components/homepage-menu/homepage-menu.component.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import "./homepage-menu.styles.scss";
 
-const HomepageMenu = ({ title, imageUrl, size }) => {
-  return (
-    <div
-      className={`directory__menu ${size}`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
-      <div className="directory__content">
-        <h1 className="directory__content__title">{title}</h1>
-        <p className="directory__content__subtitle">SHOP NOW</p>
-      </div>
+const HomepageMenu = ({ title, imageUrl, size }) => (
+  <div
+    className={`directory__menu ${size}`}
+    style={{ backgroundImage: `url(${imageUrl})` }}
+  >
+    <div className="directory__content">
+      <h1 className="directory__content__title">{title}</h1>
+      <p className="directory__content__subtitle">SHOP NOW</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default HomepageMenu;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
-const Homepage = () => {
-  return (
-    <div className="homepage-container">
-      <Directory />
-    </div>
-  );
-};
+const Homepage = () => (
+  <div className="homepage-container">
+    <Directory />
+  </div>
+);
 
 export default Homepage;
