f304413f56ebc074d801d832eb7b7dc4aee8b1ac
abid7 <abidshahriar7@gmail.com>
2020-07-14 18:28:37 +0600

    added the shop page and the header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Route, link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/collection-preview/collection-preview.components.jsx
+++ src/components/collection-preview/collection-preview.components.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
@@ -6,8 +8,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, index) => index < 4)
-        .map(({ id, name, imageUrl }) => (
-          <div key={id}>{name}</div>
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
   </div>
